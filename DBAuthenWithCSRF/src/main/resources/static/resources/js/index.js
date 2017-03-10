d7385123bab2e6e5f3f50262c8a6b607c5dd5ede
stephenking1101 <286431425@qq.com>
2017-03-10 18:31:45 +0800

    Update index.js

--- DBAuthenWithCSRF/src/main/resources/static/resources/js/index.js
+++ DBAuthenWithCSRF/src/main/resources/static/resources/js/index.js
@@ -1,4 +1,10 @@
 $(function() {
+        BrowserDetect.init();
+		console.log("You are using " + BrowserDetect.browser + " with version " + BrowserDetect.version + ".");
+		if(BrowserDetect.browser == "Explorer" && BrowserDetect.version < 9){
+			console.log("In order to use all features in the website, we suggest you to upgrade your browser.");
+			console.log("Supported browser and version are: Internet Explorer 9 or above, Chrome, Firefox, Safari and Opera");
+		}
     	 
         if (localStorage.chkbx && localStorage.chkbx != '') {
             $('#rememberMe').attr('checked', 'checked');
@@ -70,3 +76,43 @@ function openModelDialog(){
             $('#progressModal').modal({backdrop: "static", keyboard: false});
             rememberMe();
 }
+
+var BrowserDetect = {
+        init: function () {
+            this.browser = this.searchString(this.dataBrowser) || "Other";
+            this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "Unknown";
+        },
+        searchString: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var dataString = data[i].string;
+                this.versionSearchString = data[i].subString;
+
+                if (dataString.indexOf(data[i].subString) !== -1) {
+                    return data[i].identity;
+                }
+            }
+        },
+        searchVersion: function (dataString) {
+            var index = dataString.indexOf(this.versionSearchString);
+            if (index === -1) {
+                return;
+            }
+
+            var rv = dataString.indexOf("rv:");
+            if (this.versionSearchString === "Trident" && rv !== -1) {
+                return parseFloat(dataString.substring(rv + 3));
+            } else {
+                return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+            }
+        },
+        dataBrowser: [
+            {string: navigator.userAgent, subString: "Edge", identity: "MS Edge"},
+            {string: navigator.userAgent, subString: "MSIE", identity: "Explorer"},
+            {string: navigator.userAgent, subString: "Trident", identity: "Explorer"},
+            {string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
+            {string: navigator.userAgent, subString: "Opera", identity: "Opera"},  
+            {string: navigator.userAgent, subString: "OPR", identity: "Opera"},  
+            {string: navigator.userAgent, subString: "Chrome", identity: "Chrome"}, 
+            {string: navigator.userAgent, subString: "Safari", identity: "Safari"}       
+        ]
+};
